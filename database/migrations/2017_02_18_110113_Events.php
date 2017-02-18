0a8d860115984df1f4d14c485dc2bc802f75b4bd
gablooblue <gabrielhenry.lopez@gmail.com>
2017-02-18 19:14:48 +0800

    Added needed models, requests, and controllers

---
+++ database/migrations/2017_02_18_110113_Events.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Events extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	Schema::create('events', function (Blueprint $table)
+	{    
+	    $table->increments('id') ;
+	    $table->string('title');
+	    $table->longText('description');
+	    $table->integer('donations');
+	    $table->string('type');
+	    $table->integer('goal');
+	    $table->string('date');
+	    $table->string('location');
+	    $table->timestamps();
+	});	    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	Schema::dropIfExists('events');
+    }
+}
