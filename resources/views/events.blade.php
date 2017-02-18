4d559e653df726ccf675b9b9eb0f45f0f91ef4e9
Sean Ryan Chan <seanryachan@gmail.com>
2017-02-19 04:00:27 +0800

    styled splash, added markup to events

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,45 +1,41 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="panel panel-default">
+<div class="panel panel-default col-md-4">
   <div class="panel-heading">
-    <h3 class="panel-title">Panel title</h3>
+    <h2 class="panel-title">EVENT NAME</h2>
   </div>
   <div class="panel-body">
-    Panel content
+    <h3>NAME OF ORGANIZATION</h3>
+    <p>DATE</p>
+    <span class="glyphicon glyphicon-map-marker"><p>LOCATION</p></span>
+    <p>description</p>
+    <h4>Progress Tracker</h4>
     <div class="progress">
       <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
         <span class="sr-only">20% Complete</span>
       </div>
     </div>
+    <button type="button" class="btn btn-default">Pledge Now!</button>
   </div>
 </div>
-<div class="panel panel-default">
+<div class="panel panel-default col-md-4">
   <div class="panel-heading">
-    <h3 class="panel-title">Panel title</h3>
+    <h2 class="panel-title">EVENT NAME</h2>
   </div>
   <div class="panel-body">
-    Panel content
-      <div class="progress">
-        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
-          <span class="sr-only">600/1000 mL of blood</span>
-        </div>
-      </div>
-  </div>
-</div>
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title">Panel title</h3>
-  </div>
-  <div class="panel-body">
-    Panel content
+    <h3>NAME OF ORGANIZATION</h3>
+    <p>DATE</p>
+    <span class="glyphicon glyphicon-map-marker"><p>LOCATION</p></span>
+    <p>description</p>
+    <h4>Progress Tracker</h4>
     <div class="progress">
-      <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-        <span class="sr-only">8/10 Volunteers</span>
+      <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+        30 Volunteers have pledged!
       </div>
+        70 More Volunteers needed!
     </div>
+    <button type="button" class="btn btn-default">Pledge Now!</button>
   </div>
 </div>
-
-
 @endsection
--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
+<div id="myCarousel" class="carousel slide " data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -9,35 +9,39 @@
     <li data-target="#myCarousel" data-slide-to="3"></li>
   </ol>
   <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
+  <div class="carousel-inner" role="listbox" >
     <div class="item active">
-      <img src="yolanda.jpg" alt="Yolanda">
+      <img src="yolandaScaled.jpg" alt="Yolanda">
       <div class="carousel-caption">
         <h3>Aid Existing Relief Campaigns</h3>
         <p>Donate to the victims of Yolanda Right Now!</p>
+        <p><a href="{{ url('/register') }}" class="btn btn-lg btn-primary">Sign up now!</a>
       </div>
     </div>
     <div class="item">
-      <img src="coastClean.jpg" alt="Coast Cleaning">
+      <img src="coastCleanScaled.jpg" alt="Coast Cleaning">
       <div class="carousel-caption">
-        <h3>...</h3>
-        <p>...</p>
-      </div>
-    </div>
-
-    <div class="item">
-      <img src="colorRun.jpg" alt="Color Run">
-      <div class="carousel-caption">
-        <h3>...</h3>
-        <p>...</p>
+        <h3>International Coast Cleaning</h3>
+        <p>Join efforts to create a cleaner environment!</p>
+        <p><a href="{{ url('/register') }}" class="btn btn-lg btn-primary">Sign up now!</a>
       </div>
     </div>
 
     <div class="item">
-      <img src="bloodDrive.jpg" alt="Blood Drive">
+      <img src="teachScaled.jpg" alt="Color Run">
       <div class="carousel-caption">
-        <h3>...</h3>
-        <p>...</p>
+        <h3>Teach the Filipinos of Tomorrow!</h3>
+        <p>Teach for the Philippines</p>
+        <p><a href="{{ url('/register') }}" class="btn btn-lg btn-primary">Sign up now!</a>
+
+      </div>
+    </div>
+    <div class="item">
+      <img src="bloodDriveScaled.jpg" alt="Blood Drive">
+      <div class="carousel-caption">
+        <h3>Donate Blood for a Cause!</h3>
+        <p>Find Beneficiaries right now</p>
+        <p><a href="{{ url('/register') }}" class="btn btn-lg btn-primary">Sign up now!</a>
       </div>
     </div>
   </div>
@@ -52,27 +56,29 @@
   </a>
 </div>
 
-<div class="row" style="padding-top: 1.5em">
-  <div class="col-md-6 col-md-1">
-    <div class="thumbnail">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <h1 class="text-center">Thumbnail label</h1>
-        <p>DESCRIPTION</p>
-      </div>
+<div class="container" style="padding-top: 1.5em">
+  <h1 class="text-center">Team Codix</h1>
+  <div class="row" style="padding-top: 1.5em">
+    <div class="col-lg-4">
+      <img class="img-circle">
+      <h2 class="text-center">Gab Lopez</h2>
+      <p class="text-center">DESCRIPTION</p>
     </div>
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <h1 class="text-center">Thumbnail label</h1>
-        <p>DESCRIPTION</p>
+    <div class="col-lg-4">
+      <img class="img-circle">
+      <h2 class="text-center">Aaron Ramos</h2>
+      <p class="text-center">DESCRIPTION</p>
+    </div>
+    <div class="col-lg-4">
+      <img class="img-circle">
+      <h2 class="text-center">Makki Villaluz</h2>
+      <p class="text-center">DESCRIPTION</p>
+    </div>
+    <div class="col-lg-4">
+      <img class="img-circle">
+      <h2 class="text-center">Sean Ryan Chan</h2>
+      <p class="text-center">DESCRIPTION</p>
     </div>
   </div>
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <h1 class="text-center">Thumbnail label</h1>
-        <p>DESCRIPTION</p>
-    </div>
-  </div>
-  </div>
+</div>
 @endsection
